68abe3c35bb4e5d03c28ff5c5e8aecb7e0060299
leo <leo@MacBook-Air-de-leo.local>
2025-09-13 16:40:13 +0200

    fixed output of get_athletes_stats

--- strava_tools.py
+++ strava_tools.py
@@ -25,12 +25,11 @@ client = Client(access_token=token)
 )
 def Get_Athletes_Stats() -> str :
     '''
-    
     Output : A JSON Containing all the stats of the current user
     '''
     athlete_id = client.get_athlete().id # APi call
     ahtlete_stats = client.get_athlete_stats(athlete_id)
     dict = {"recent_run_totals" : ahtlete_stats.recent_run_totals.model_dump_json(), "ytd_run_totals" : ahtlete_stats.ytd_run_totals.model_dump_json(), "all_run_totals" : ahtlete_stats.all_run_totals.model_dump_json()}
     
-    return dict
+    return str(dict)
 
