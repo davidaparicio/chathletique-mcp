a480d2384e6e104ff783f0ba701c9707af92177b
leo <leo@MacBook-Air-de-leo.local>
2025-09-13 20:40:44 +0200

    fixed mcp server redefinition

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ MCP Server Template
 from strava_tools import get_athletes_stats, get_last_runs, create_itinerary
 from weather_tools import get_weather_prediction
 
-from mcp.server.fastmcp import FastMCP
 from mcp_utils import mcp
-
-mcp = FastMCP("Echo Server", port=3000, stateless_http=True, debug=True)
 
 
 
