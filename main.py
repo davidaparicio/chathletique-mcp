ae36c0654be0a0300c493ba88a97833b98b115d7
leo <leo@MacBook-Air-de-leo.local>
2025-09-13 17:56:59 +0200

    weather tools added in main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ MCP Server Template
 """
 
 from mcp_utils import mcp
-from strava_tools import mcp, get_athletes_stats, get_last_runs
-
-
+from strava_tools import get_athletes_stats, get_last_runs
+from weather_tools import get_weather_prediction
 
 
 if __name__ == "__main__":
