06a9dc9bc974ef5833fbeb0eb9a1c47aae57ccda
Lucie Sénéclauze <lseneclauze04@yahoo.com>
2025-09-14 16:48:14 +0800

    no longer look at user's average position

--- weather_tools.py
+++ weather_tools.py
@@ -2,6 +2,8 @@ import requests
 import os
 from mcp_utils import mcp
 from dotenv import load_dotenv
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 
 
 # -------------------------------- Globals --------------------------------
@@ -19,24 +21,18 @@ if not token:
     title="Get Weather Predictions",
     description="Return some future weather informations for where the user lives. the place where the user lives is found by looking at where previous runs is located ",
 )
-def get_weather_prediction()-> str:
+def get_weather_prediction(place_name : str)-> str:
 
     """
     Loads positions from run_positions.txt and returns weather forecast as a dict.
     """
-    try:
-        with open("run_positions.txt", "r") as f:
-            positions = [line.strip() for line in f if line.strip()]
-    except FileNotFoundError:
-        print("Error: run_positions.txt not found. Please generate run positions first.")
-        return None
 
     base_url = "http://api.openweathermap.org/data/2.5/forecast"
 
-    average_location = get_user_localisation(positions)
+    longitude, latitude = _get_coordinates(place_name)
     params = {
-    "lat": round(average_location[0], 2),
-    "lon": round(average_location[1], 2),
+    "lat": longitude,
+    "lon": latitude,
     "appid" : token,
     "exclude" : "current,minutely,alerts",
 
@@ -49,20 +45,21 @@ def get_weather_prediction()-> str:
 
 
 # -------------------------------- Useful functions --------------------------------
-
-# Look at the coordinates of the 20 last runs to create an average latitude and longitude which will be used to get the weather
-def get_user_localisation(lines:list)->tuple:
-    #preprocess positions from strings to tuples
-    positions = []
-    for line in lines:
-        # Remove "root=" and brackets, then split by comma
-        numbers = line.strip().replace("root=", "").strip("[]")
-        lat, lon = map(float, numbers.split(","))
-        positions.append([lat, lon])
-    x_sum = sum(t[0] for t in positions)
-    y_sum = sum(t[1] for t in positions)
-    n = len(positions)
-    return (x_sum / n, y_sum / n)
+# get the coordinates of a place name
+def _get_coordinates(place_name : str) -> tuple:
+    """
+    Get the coordinates of a place name
+    """
+    geolocator = Nominatim(user_agent="my_geocoder_app")
+    try:
+        location = geolocator.geocode(place_name)
+    except (GeocoderTimedOut, GeocoderServiceError) as e:
+        print("Error:", e)
+        return 'Failed to get coordinates'
+    if location:
+        return (location.longitude, location.latitude)
+    else:
+        return 'Failed to get coordinates'
 
 
 
