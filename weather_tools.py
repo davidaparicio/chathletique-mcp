2872e06c31e415f7f9c89f4e4e29db2ed310a5d4
Lucie Sénéclauze <lseneclauze04@yahoo.com>
2025-09-14 16:19:02 +0800

    fix typo

--- weather_tools.py
+++ weather_tools.py
@@ -17,7 +17,7 @@ if not token:
 # -------------------------------- Tools --------------------------------
 @mcp.tool(
     title="Get Weather Predictions",
-    description="Return some future weather informations for where the user leaves. the place where the user lives is found by looking at where previous runs is located ",
+    description="Return some future weather informations for where the user lives. the place where the user lives is found by looking at where previous runs is located ",
 )
 def get_weather_prediction()-> str:
 
